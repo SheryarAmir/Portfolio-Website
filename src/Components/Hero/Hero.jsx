00d70a1aa857c3976e7f40fa0f027260d73ddfed
SheryarAmir <dev.sheryaramir@gmail.com>
2024-08-01 17:46:30 +0500

    complete the website

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ import AnchorLink from 'react-anchor-link-smooth-scroll'
 const Hero = () => {
   return (
     <div id='home' className='hero'>
-      <img src={profile_img} alt="" />
+      <img  src={profile_img}   alt="" />
       <h1><span>I'm Sheryar Amir,</span> Front end Developer based in Gilgit Pakistan.</h1>
       <p>I am front end developer from Gilgit Pakistan. with 3 Years of experince im multiple companies like Google, Meta, Apple, and Teala.</p>
       <div className="hero-action">
